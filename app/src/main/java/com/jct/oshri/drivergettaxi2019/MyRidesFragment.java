a08c9dfe05f3290bbe74d39db3a635a08c0c1eac
אלון אלימלך <aloni12345@gmail.com>
2019-01-19 21:49:51 +0200

    view list

---
+++ app/src/main/java/com/jct/oshri/drivergettaxi2019/MyRidesFragment.java
@@ -0,0 +1,44 @@
+package com.jct.oshri.drivergettaxi2019;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.jct.oshri.drivergettaxi2019.model.backend.DB_manager;
+import com.jct.oshri.drivergettaxi2019.model.backend.factoryMethod;
+import com.jct.oshri.drivergettaxi2019.model.datasource.FireBase_DBManager;
+import com.jct.oshri.drivergettaxi2019.model.entities.Ride;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyRidesFragment extends Fragment {
+    Ride rides;
+    DB_manager dBase;
+
+    public MyRidesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_my_rides, container, false);
+        dBase = factoryMethod.getManager();
+        List<Ride> unOccupiedRide = ((FireBase_DBManager)dBase).getUnoccupiedRides();
+
+        ListView listRides = view.findViewById(R.id.ride_list);
+
+        return view;
+    }
+
+}
